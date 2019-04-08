e3ab3f4276f1958fcee6113160736f5606d0c30d
wei.wang <qxwycyww@gmail.com>
2019-04-08 23:39:31 +0800

    spring-jpa

---
+++ spring-learn/src/main/java/com/xpand/spring/SpringBootAppStarter.java
@@ -0,0 +1,17 @@
+package com.xpand.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wei.wang
+ * @Description:
+ * @Date 2019/4/8 22:10
+ * @Modified
+ */
+@SpringBootApplication
+public class SpringBootAppStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppStarter.class, args);
+    }
+}
---
+++ spring-learn/src/main/java/com/xpand/spring/jpa/entity/Coffee.java
@@ -0,0 +1,48 @@
+package com.xpand.spring.jpa.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.joda.money.Money;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: wei.wang
+ * @Description:
+ * @Date 2019/4/8 22:32
+ * @Modified
+ */
+@Entity
+@Table(name = "T_MENU")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee implements Serializable {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    @Column
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmount",
+            parameters ={@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
+    private Money price;
+    @Column(updatable = false)
+    @CreationTimestamp
+    private Date createTime;
+    @UpdateTimestamp
+    private Date updateTime;
+}
