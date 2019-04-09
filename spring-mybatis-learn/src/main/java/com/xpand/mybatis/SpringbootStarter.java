937f1b4d01b7e5d3b0060d0162e4218df547d186
wei.wang <qxwycyww@gmail.com>
2019-04-09 23:18:25 +0800

    mybatis

---
+++ spring-mybatis-learn/src/main/java/com/xpand/mybatis/SpringbootStarter.java
@@ -0,0 +1,17 @@
+package com.xpand.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wei.wang
+ * @Description:
+ * @Date 2019/4/9 22:28
+ * @Modified
+ */
+@SpringBootApplication
+public class SpringbootStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStarter.class);
+    }
+}
