039a0895df9dc4c24bf263130d55993abd230790
wei.wang <362686446@qq.com>
2019-04-11 11:01:53 +0800

    jpa

---
+++ spring-mybatis-learn/src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+set sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,ALLOW_INVALID_DATES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+
+drop table if exists t_coffee ;
+drop table if exists t_order;
+drop table if exists t_order_coffee;
+
+create table t_coffee (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    name varchar(255),
+    price bigint,
+    primary key (id)
+);
+
+create table t_order (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    customer varchar(255),
+    state integer not null,
+    primary key (id)
+);
+
+create table t_order_coffee (
+    coffee_order_id bigint not null,
+    items_id bigint not null
+);
+
+insert into t_coffee (name, price, create_time, update_time) values ('espresso', 2000, now(), now());
+insert into t_coffee (name, price, create_time, update_time) values ('latte', 2500, now(), now());
+insert into t_coffee (name, price, create_time, update_time) values ('capuccino', 2500, now(), now());
+insert into t_coffee (name, price, create_time, update_time) values ('mocha', 3000, now(), now());
+insert into t_coffee (name, price, create_time, update_time) values ('macchiato', 3000, now(), now());
